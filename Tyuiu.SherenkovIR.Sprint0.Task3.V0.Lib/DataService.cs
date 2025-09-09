f57e8ccc5c4ef2f4311730f3eee1475d0d84f935
Tyuiu.SherenkovIR@outlook.com <Sherenkov12@gmail.com>
2025-09-09 18:34:09 +0500

    Task3

---
+++ Tyuiu.SherenkovIR.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.SherenkovIR.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+
+        {  
+            return a + b; 
+        }
+
+
+     }
+
+}
